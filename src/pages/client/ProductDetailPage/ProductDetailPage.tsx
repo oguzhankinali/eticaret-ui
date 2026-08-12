ff0924133e0424cf6a9dcc47e367bf78d27a02a7
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-12 22:12:03 +0300

    refactor: integrate HttpClientService into ProductDetailPage

--- src/pages/client/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/client/ProductDetailPage/ProductDetailPage.tsx
@@ -1,12 +1,42 @@
+import { HttpClientService } from '../../../services/httpclient.service';
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom"
-
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    stock: number;
+}
+const httpClientService = new HttpClientService();
 export default function ProductDetailPage() {
     const { id } = useParams<{ id: string }>()
+    const [product, setProduct] = useState<Product | null>(null);
+    const getProduct = async () => {
+        try {
+            const data = await httpClientService.get<Product>({ controller: "products", action: id });
+            setProduct(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        if (id) {
+            getProduct();
+        }
+    }, [id]);
     const navigate = useNavigate()
     return (
         <div className="detail-container">
-            <h2>Ürün Detay Sayfası</h2>
-            <p>Şu an incelenen ürünün ID değeri: <strong>{id}</strong></p>
+            {product ?
+                <div>
+                    <h2>Ürün Detay Sayfası</h2>
+                    <p>Ürün İsmi: <strong>{product.name}</strong></p>
+                    <p>Stokta Bulunan: <strong>{product.stock}</strong></p>
+                    <p>Ürün Fiyatı: <strong>{product.price}</strong></p>
+
+                </div>
+                : <p>Ürün bulunamadı</p>
+            }
 
             <div className="button-group">
                 <button
