1a778cf2fd1aa2888f99d6e0c382a30fd8086abb
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-12 17:29:17 +0300

    feat: implement HttpClientService with CRUD methods

---
+++ src/services/httpclient.service.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+export interface RequestParameters {
+    controller: string;
+    action?: string;
+    queryString?: string;
+    headers?: any;
+    baseUrl?: string;
+    fullPath?: string;
+}
+export class HttpClientService {
+    private baseUrl: string = "https://localhost:7083/api";
+    private url(requestParameter: Partial<RequestParameters>): string {
+        if (requestParameter.fullPath)
+            return requestParameter.fullPath;
+
+        const base: string = requestParameter.baseUrl ? requestParameter.baseUrl : this.baseUrl;
+
+        let fullUrl: string = `${base}/${requestParameter.controller}`;
+
+        if (requestParameter.action)
+            fullUrl += `/${requestParameter.action}`;
+
+        return fullUrl;
+    }
+    async get<T>(requestParameter: Partial<RequestParameters>): Promise<T> {
+        let reqUrl: string = "";
+        if (requestParameter.fullPath)
+            reqUrl = requestParameter.fullPath;
+        else
+            reqUrl = `${this.url(requestParameter)}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
+
+        const response = await axios.get<T>(reqUrl, { headers: requestParameter.headers });
+        return response.data;
+    }
+    async post<T, R>(requestParameter: Partial<RequestParameters>, body: Partial<T>): Promise<R> {
+        let reqUrl: string = "";
+        if (requestParameter.fullPath)
+            reqUrl = requestParameter.fullPath;
+        else
+            reqUrl = `${this.url(requestParameter)}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
+        const response = await axios.post<R>(reqUrl, body, { headers: requestParameter.headers });
+        return response.data
+    }
+    async put<T, R>(requestParameter: Partial<RequestParameters>, body: Partial<T>): Promise<R> {
+        let reqUrl: string = "";
+        if (requestParameter.fullPath)
+            reqUrl = requestParameter.fullPath;
+        else
+            reqUrl = `${this.url(requestParameter)}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
+
+        const response = await axios.put<R>(reqUrl, body, { headers: requestParameter.headers });
+        return response.data;
+    }
+    async delete<R>(requestParameter: Partial<RequestParameters>, id: string): Promise<R> {
+        let reqUrl: string = "";
+        if (requestParameter.fullPath)
+            reqUrl = requestParameter.fullPath;
+        else
+            reqUrl = `${this.url(requestParameter)}/${id}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
+        const response = await axios.delete<R>(reqUrl, { headers: requestParameter.headers });
+        return response.data;
+    }
+}
