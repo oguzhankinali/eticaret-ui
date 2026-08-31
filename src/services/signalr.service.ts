446afcc0adb3c7d85ec2ebea1b5c92d1de3b4302
Oguzhan Kinali <oguzhankinali07@gmail.com>
2026-08-31 22:37:27 +0300

    refactor(signalr): prevent duplicate connections with state check

--- src/services/signalr.service.ts
+++ src/services/signalr.service.ts
@@ -1,21 +1,24 @@
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from "@microsoft/signalr";
 
 export class SignalRService {
-
+    private _connection?: HubConnection;
 
     start(hubUrl: string): HubConnection {
-        const builder: HubConnectionBuilder = new HubConnectionBuilder();
-        const hubConnection: HubConnection = builder
-            .withUrl(hubUrl)
-            .withAutomaticReconnect()
-            .build();
+        if (!this._connection || this._connection.state === HubConnectionState.Disconnected) {
+            const builder: HubConnectionBuilder = new HubConnectionBuilder();
+            const hubConnection: HubConnection = builder
+                .withUrl(hubUrl)
+                .withAutomaticReconnect()
+                .build();
 
-        hubConnection.start()
-            .then(() => console.log("SignalR bağlantısı başarıyla kuruldu."))
-            .catch((error) => {
-                console.error("SignalR bağlantı hatası: ", error);
-                setTimeout(() => this.start(hubUrl), 2000);
-            })
+            hubConnection.start()
+                .then(() => console.log("SignalR bağlantısı başarıyla kuruldu."))
+                .catch((error) => {
+                    console.error("SignalR bağlantı hatası: ", error);
+                    setTimeout(() => this.start(hubUrl), 2000);
+                })
+        }
+
         return hubConnection;
     }
 
